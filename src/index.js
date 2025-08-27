d35f08ecefdf760e7b6653379d33bbafdccd902d
Carlos Renier <carl.perezv@hotmail.com>
2025-08-27 00:58:32 -0400

    Antes de deploy

--- src/index.js
+++ src/index.js
@@ -4,12 +4,6 @@ import app from "./app.js";
 dotenv.config();
 
 const PORT = process.env.PORT || 4000;
-const HOST = "0.0.0.0";
-
-app.listen(PORT, HOST, () => {
-    console.log(`[babyjo-back] escuchando en http://${HOST}:${PORT}`);
-    console.log(`[env] NODE_ENV=${process.env.NODE_ENV || "development"}`);
-    if (process.env.CORS_ORIGIN) {
-        console.log(`[cors] CORS_ORIGIN=${process.env.CORS_ORIGIN}`);
-    }
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
